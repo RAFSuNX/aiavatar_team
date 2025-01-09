aed588f255ce14dfeecee9b529c90879606a4228
RAFSuNX <git@rafsunx.com>
2025-01-09 13:13:50 +0600

    build error fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: "export", // Ensures static HTML files are generated
+    distDir: "out",   // Sets the output directory for static files
+  };
+  
+  export default nextConfig;
+  
